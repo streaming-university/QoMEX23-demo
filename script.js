0926a5eea376ae16c2dff632458bc59798b3618f
Burak Kara <burak.kara@ozu.edu.tr>
2023-05-29 14:49:28 +0200

    fix style

--- script.js
+++ script.js
@@ -4,17 +4,12 @@ const video2Label = document.getElementById('video2-text');
 const startBtn = document.getElementById('start-stop-btn');
 const reset = document.getElementById('restart-btn');
 const mute = document.getElementById('mute-btn');
-const timeElapsed = document.getElementById('time-elapsed');
-const duration = document.getElementById('duration');
-const seekBar = document.getElementById('seek-bar');
-const seekTooltip = document.getElementById('seek-tooltip');
-const seek = document.getElementById('seek');
 const videoSelect = document.getElementById('video-select');
 const qualitySelect = document.getElementById('quality-select');
 const comparisonSelect = document.getElementById('comparison-select');
 
 const videos = {
-  bbb: {
+  "bbb": {
     qualities: {
       20: {
         //https://drive.google.com/file/d/1glgLM4dVw7O0ls3QI8R-CL69whL8HjLl/view?usp=share_link
@@ -68,7 +63,7 @@ const videos = {
       }
     }
   },
-  motorbike: {
+  "motorbike": {
     qualities: {
       20: {
         //https://drive.google.com/file/d/1_mu1-BS810uLxzuZrK67d1AQdWM-iVbg/view?usp=share_link
@@ -97,79 +92,22 @@ const videos = {
   }
 };
 
-
-
-// video1.src = `./videos/spliced/${videoSelect.value}/${qualitySelect.value}.mp4`;
-// video2.src = `./videos/original/${videoSelect.value}/20.mp4`;
-
 video1.src = videos[videoSelect.value].qualities[qualitySelect.value].spliced;
 video2.src = videos[videoSelect.value].qualities[qualitySelect.value].original;
 video2Label.textContent = 'Reference Video: High Quality (QP 20)';
 
-// Helpers
-const formatTime = (timeInSeconds) => ({
-  minutes: Math.floor(timeInSeconds / 60), seconds: timeInSeconds % 60
-});
-
-const str_pad_left = (string, pad, length) => (new Array(length + 1).join(pad) + string).slice(-length);
-
-// Controllers
-const initializeVideo = () => {
-  const videoDuration = Math.round(video1.duration);
-  seek.setAttribute('max', videoDuration);
-  const time = formatTime(videoDuration);
-  duration.innerText = `${time.minutes}:${time.seconds}`;
-  duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`);
-};
-
-const updateProgress = () => {
-  seek.value = Math.floor(video1.currentTime);
-};
-
-const updateTimeElapsed = () => {
-  const time = formatTime(Math.round(video1.currentTime));
-  timeElapsed.innerText = str_pad_left(time.minutes, '0', 2) + ':' + str_pad_left(time.seconds, '0', 2);
-  timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`);
-};
-
-const updateSeekTooltip = (event) => {
-  const skipTo = Math.round((event.offsetX / event.target.clientWidth) * parseInt(event.target.getAttribute('max'), 10));
-  seek.setAttribute('data-seek', skipTo);
-  const t = formatTime(skipTo);
-  seekTooltip.textContent = `${t.minutes}:${t.seconds}`;
-  const rect = video1.getBoundingClientRect();
-  seekTooltip.style.left = `${event.pageX - rect.left}px`;
-};
-
-const skipAhead = (event) => {
-  const skipTo = event.target.dataset.seek ? event.target.dataset.seek : event.target.value;
-  video1.currentTime = skipTo;
-  video2.currentTime = skipTo;
-  seek.value = skipTo;
-};
-
-if (video1.readyState >= video1.HAVE_FUTURE_DATA) initializeVideo(); else video1.addEventListener('canplay', initializeVideo);
-
 // Event Listeners
-video1.addEventListener('timeupdate', updateTimeElapsed);
-video1.addEventListener('timeupdate', updateProgress);
-seek.addEventListener('mousemove', updateSeekTooltip);
-seek.addEventListener('input', skipAhead);
-
-
 video1.addEventListener('readystatechange', function() {
-  // console.log('Ready state updated: ' + videoElement.readyState);
-  if (video1.readyState >= 4 && video2.readyState >= 4) {
-    alert("Both videos are loaded")
-  }
-});
-video2.addEventListener('readystatechange', function() {
-  // console.log('Ready state updated: ' + videoElement.readyState);
   if (video1.readyState >= 4 && video2.readyState >= 4) {
     alert("Both videos are loaded")
   }
 });
 
+video2.addEventListener('readystatechange', function() {
+  if (video1.readyState >= 4 && video2.readyState >= 4) {
+    alert("Both videos are loaded")
+  }
+});
 
 videoSelect.addEventListener('change', () => {
   video1.src = videos[videoSelect.value].qualities[qualitySelect.value].spliced;
